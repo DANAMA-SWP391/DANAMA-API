c5e82f45b1178e36973f36e426104030b43db8cd
CheeseMink <mjnhtran2004@gmail.com>
2024-10-03 16:20:35 +0700

    Update method getComingSoonMovies

--- src/main/java/repository/MovieDAO.java
+++ src/main/java/repository/MovieDAO.java
@@ -66,9 +66,58 @@ public class MovieDAO extends DBContext {
 
         return nowPlayingMovies;
     }
+    // Method to get movies that are coming soon (status = 2 or based on releaseDate)
+    public List<Movie> getComingSoonMovies() {
+        List<Movie> comingSoonMovies = new ArrayList<>();
+        String sql = "SELECT * FROM Movie WHERE status = 2"; // or use a condition based on releaseDate
+
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            // Using the connection from DBContext
+            statement = connection.prepareStatement(sql);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int movieId = resultSet.getInt("movieId");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                String poster = resultSet.getString("poster");
+                String trailer = resultSet.getString("trailer");
+                Date releaseDate = resultSet.getDate("releasedate");
+                String country = resultSet.getString("country");
+                String director = resultSet.getString("director");
+                int ageRestricted = resultSet.getInt("agerestricted");
+                String actors = resultSet.getString("actors");
+                int duration = resultSet.getInt("duration");
+                int status = resultSet.getInt("status");
+
+                // Get genres using GenreDAO
+                List<Genre> genres = genreDAO.getGenresByMovieId(movieId);
+
+                // Create a new Movie object and add it to the list
+                Movie movie = new Movie(movieId, name, description, poster, trailer, releaseDate, country, director, ageRestricted, actors, duration, status, genres);
+                comingSoonMovies.add(movie);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return comingSoonMovies;
+    }
 
     public static void main(String[] args) {
         MovieDAO movieDAO = new MovieDAO();
         System.out.println(movieDAO.getNowPlayingMovies());
+        System.out.println(movieDAO.getComingSoonMovies());
     }
 }
