3e2117332f74557bf7e26f9fba8545fd3275e31b
Nguyen Van Duy Thang <bithang28@gmail.com>
2024-10-06 16:01:32 +0700

    ChangeSeatType (SeatDAO), getListShowtimeByCinemaID, getListSHowtimeByRoom (ShowtimeDAO) , getListRoomsByCinemaID (RoomDAO)

--- src/main/java/repository/BookingDAO.java
+++ src/main/java/repository/BookingDAO.java
@@ -5,6 +5,20 @@
 package repository;
 
 import context.DBContext;
+import model.Booking;
+import model.Showtime;
+import model.Room;
+import model.Account;
+import model.Cinema;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class BookingDAO extends DBContext{
-    
+
+    public static void main(String[] args) {
+
+    }
 }
