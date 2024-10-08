73774fa7ac1e180661246ba0bb3de37df165aae7
penguink9 <giangqt2k4@gmail.com>
2024-10-08 15:52:01 +0700

    Create controller servlets

---
+++ src/main/java/controller/user/BookingController.java
@@ -0,0 +1,22 @@
+package controller.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "BookingController", value = "/BookingController")
+public class BookingController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
