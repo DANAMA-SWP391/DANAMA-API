5514a0c7be82d435f86b37abbf0cc6a58c16c0e7
CheeseMink <mjnhtran2004@gmail.com>
2024-10-20 21:12:21 +0700

    modify AccountController, CinemaController, CinemaDAO, GenreDAO, MovieController, MovieDAO

--- src/main/java/controller/admin/AccountController.java
+++ src/main/java/controller/admin/AccountController.java
@@ -36,34 +36,48 @@ public class AccountController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setCharacterEncoding("UTF-8");
-       Gson gson = new Gson();
-       JsonObject jsonObject = gson.fromJson(request.getReader(), JsonObject.class);
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(request.getReader(), JsonObject.class);
         String action = jsonObject.get("action").getAsString();
         System.out.println(jsonObject);
         AccountDAO dao = new AccountDAO();
         boolean result = false;
+        JsonObject jsonResponse = new JsonObject();
 
         switch (action) {
             case "add":
                 Account account = gson.fromJson(jsonObject.get("account"), Account.class);
                 result = dao.addAccount(account);
                 System.out.println(account);
+                jsonResponse.addProperty("success", result);
                 break;
+
             case "view":
                 Account viewAccount = gson.fromJson(jsonObject.get("account"), Account.class);
+                System.out.println("Viewing account with UID: " + viewAccount.getUID());
                 Account accountDetails = dao.getAccountById(viewAccount.getUID());
-                break;
+
+                if (accountDetails != null) {
+                    jsonResponse.addProperty("success", true);
+                    jsonResponse.add("account", gson.toJsonTree(accountDetails)); // Trả về chi tiết tài khoản
+                } else {
+                    jsonResponse.addProperty("success", false);
+                    jsonResponse.addProperty("message", "Account not found.");
+                }
+                response.getWriter().write(gson.toJson(jsonResponse));  // Gửi phản hồi chi tiết tài khoản
+                return; // Dừng ở đây sau khi gửi phản hồi cho case "view"
+
             case "ban":
                 Account accountBan = gson.fromJson(jsonObject.get("account"), Account.class);
                 result = dao.banAccount(accountBan.getUID());
+                jsonResponse.addProperty("success", result);
                 break;
         }
 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("{\"success\":" + result + "}");
+        response.getWriter().write(gson.toJson(jsonResponse));  // Gửi phản hồi JSON cho các case khác
         response.getWriter().flush();
         response.getWriter().close();
     }
-
 }
