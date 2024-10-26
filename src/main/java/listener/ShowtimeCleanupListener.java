7779f456a14a100057599f50e41961fb401dafb9
penguink9 <giangqt2k4@gmail.com>
2024-10-27 03:47:08 +0700

    Add clean up for Showtime,Movie,Booking

--- src/main/java/listener/ShowtimeCleanupListener.java
+++ src/main/java/listener/ShowtimeCleanupListener.java
@@ -1,4 +1,4 @@
-package repository;
+package listener;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
@@ -6,19 +6,21 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class BookingCleanupListener implements ServletContextListener {
+public class ShowtimeCleanupListener implements ServletContextListener {
 
     private ScheduledExecutorService scheduler;
 
     @Override
     public void contextInitialized(ServletContextEvent event) {
         scheduler = Executors.newSingleThreadScheduledExecutor();
-        scheduler.scheduleAtFixedRate(new BookingCleanupTask(), 0, 1, TimeUnit.MINUTES); // Check every minute
+
+        // Schedule the ShowtimeCleanupTask to run every 24 hours
+        scheduler.scheduleAtFixedRate(new ShowtimeCleanupTask(), 0, 12, TimeUnit.HOURS); // Daily check
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent event) {
         scheduler.shutdownNow();
+        System.out.println("ShowtimeCleanupListener stopped.");
     }
 }
-
